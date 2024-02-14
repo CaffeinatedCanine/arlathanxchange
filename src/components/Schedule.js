a788336df9bab22ea7dab38d2688edae6116e4ea
enigmalea <enigmalea@gmail.com>
2024-02-14 05:19:45 -0500

    updated 2024 info.

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,14 +4,14 @@ import localizedFormat from "dayjs/plugin/localizedFormat";
 
 dayjs.extend(localizedFormat);
 
-const nominationsOpen = dayjs.unix(1678636800).format("LLLL");
-const nominationsClose = dayjs.unix(1679760000).format("LLLL");
-const signUpsOpen = dayjs.unix(1679846400).format("LLLL");
-const signUpsClose = dayjs.unix(1681056000).format("LLLL");
-const assignments = dayjs.unix(1681257600).format("LLLL");
-const worksDue = dayjs.unix(1684684800).format("LLLL");
-const worksRevealed = dayjs.unix(1685289600).format("LLLL");
-const creatorsRevealed = dayjs.unix(1685894400).format("LLLL");
+const nominationsOpen = dayjs.unix(1710086400).format("LLLL");
+const nominationsClose = dayjs.unix(1711296000).format("LLLL");
+const signUpsOpen = dayjs.unix(1711296000).format("LLLL");
+const signUpsClose = dayjs.unix(1712505600).format("LLLL");
+const assignments = dayjs.unix(1712707200).format("LLLL");
+const worksDue = dayjs.unix(1716134400).format("LLLL");
+const worksRevealed = dayjs.unix(1716739200).format("LLLL");
+const creatorsRevealed = dayjs.unix(1717344000).format("LLLL");
 
 export default function Schedule() {
   return (
