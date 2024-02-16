b16c161f18ff8d12d47ac6287f46f489199dabb3
enigmalea <73132777+enigmalea@users.noreply.github.com>
2024-02-15 23:23:21 -0500

    2024-updates (#24)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,12 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
+
+const copyright =
+  "<a href='https://thehangedmanpub.netlify.app'>The Hanged Man</a>";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -12,7 +16,7 @@ const config = {
   url: "https://arlathanxchange.neocities.org",
   baseUrl: "/",
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenMarkdownLinks: "throw",
   favicon: "img/icon.png",
   titleDelimiter: "|",
   plugins: [require.resolve("docusaurus-plugin-image-zoom")],
@@ -24,8 +28,18 @@ const config = {
         docs: {
           breadcrumbs: false,
           sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsed: false,
         },
-        blog: false,
+        blog: {
+          blogTitle: "Announcements",
+          blogDescription:
+            "See the latest announcements about Arlathan eXchange. Get exchange news and mod updates.",
+          showReadingTime: true,
+          feedOptions: {
+            type: "all",
+            copyright: `Copyright © ${new Date().getFullYear()} ${copyright}`,
+          },
+        },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -33,212 +47,193 @@ const config = {
     ],
   ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: "Arlathan eXchange",
-				logo: {
-					alt: "DA:I Dialogue Wheel Elf Logo",
-					src: "img/icon.png",
-				},
-				items: [
-					{
-						type: "dropdown",
-						label: "Info",
-						position: "left",
-						className: "info",
-						items: [
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Arlathan eXchange",
+        logo: {
+          alt: "DA:I Dialogue Wheel Elf Logo",
+          src: "img/icon.png",
+        },
+        items: [
+          {
+            type: "dropdown",
+            label: "Info",
+            position: "left",
+            className: "info",
+            items: [
+              {
+                type: "doc",
+                label: "Rules & Information",
+                docId: "intro",
+              },
+              {
+                type: "doc",
+                label: "FAQs",
+                docId: "faqs",
+              },
 							{
-								type: "doc",
-								label: "Basic",
-								docId: "intro",
-							},
+                type: "doc",
+                label: "Treats & Treatless FAQs",
+                docId: "treats",
+              },
 							{
-								type: "doc",
-								label: "FAQs",
-								docId: "faqs",
-							},
-							{
-								label: "2023 Tag Set",
-								href: "https://archiveofourown.org/tag_sets/14455",
-							},
-							{
-								label: "2023 Nominations Sheet",
-								href:
-									"https://docs.google.com/spreadsheets/d/1ruHKiXgFvfz54Q5m1K7wIMvVpbfI5k-_dk5mvxr9Sv8/edit?usp=sharing",
-							},
-							{
-								label: "2023 AutoAO3: Requests",
-								href:
-									"https://autoao3app.fandom.tools/#/arlathanxchange2023/all",
-							},
-							{
-								label: "Request an Extension",
-								href:
-									"/extension",
-							},
-						],
-					},
-					{
-						type: "dropdown",
-						label: "Tutorials",
-						position: "left",
-						className: "tutorial",
-						items: [
-							{
-								type: "doc",
-								label: "How to Nominate",
-								docId: "tutorials/nominate",
-							},
-							{
-								type: "doc",
-								label: "How to Sign-Up",
-								docId: "tutorials/signups",
-							},
-							{
-								type: "doc",
-								label: "On Exchange Letters",
-								docId: "tutorials/letters",
-							},
-							{
-								type: "doc",
-								label: "How to Filter the AutoAO3 App",
-								docId: "tutorials/autoao3",
-							},
-							{
-								type: "doc",
-								label: "How to Fulfill Your Assignment",
-								docId: "tutorials/fulfill",
-							},
-							{
-								type: "doc",
-								label: "Keeping It Anon",
-								docId: "tutorials/anon",
-							},
-						],
-					},
-					{
-						href:
-							"https://archiveofourown.org/collections/arlathanxchange2023/profile",
-						position: "left",
-						label: "AO3",
-						className: "ao3",
-					},
-					{
-						href: "https://arlathanxchange.tumblr.com",
-						position: "left",
-						label: "Tumblr",
-						className: "tumblr",
-					},
-					{
-						href: "https://twitter.com/ArlathanXchange",
-						position: "left",
-						label: "Twitter",
-						className: "twitter",
-					},
-					{
-						href: "https://discord.gg/F73NDYMeGC",
-						position: "left",
-						label: "Discord",
-						className: "discord",
-					},
-				],
-			},
-			footer: {
-				style: "dark",
-				links: [
-					{
-						title: "Info",
-						items: [
-							{
-								label: "Basics",
-								href: "/docs/intro",
-							},
-							{
-								label: "FAQs",
-								to: "/docs/faqs",
-							},
-							{
-								label: "Nominations Sheet",
-								href:
-									"https://docs.google.com/spreadsheets/d/1ruHKiXgFvfz54Q5m1K7wIMvVpbfI5k-_dk5mvxr9Sv8/edit?usp=sharingg",
-							},
-							{
-								label: "Requests",
-								href:
-									"https://autoao3app.fandom.tools/#/arlathanxchange2023/all",
-							},
-						],
-					},
-					{
-						title: "Tutorials",
-						items: [
-							{
-								label: "How to Nominate",
-								to: "/docs/tutorials/nominate",
-							},
-							{
-								label: "How to Sign-Up",
-								to: "/docs/tutorials/signups",
-							},
-							{
-								label: "On Exchange Letters",
-								to: "/docs/tutorials/letters",
-							},
-							{
-								label: "How to Filter the AutoAO3 App",
-								to: "/docs/tutorials/autoao3",
-							},
-						],
-					},
-					{
-						title: "Contact",
-						items: [
-							{
-								label: "Tumblr Ask",
-								to: "https://arlathanxchange.tumblr.com/ask",
-							},
-							{
-								href: "https://discord.gg/F73NDYMeGC",
-								label: "Discord",
-							},
-							{
-								label: "arlathanxchange@gmail.com",
-								href: "mailto: arlathanxchange@gmail.com",
-							},
-						],
-					},
-					{
-						title: "Mods",
-						items: [
-							{
-								html:
-									'<div class="avatar"><img class="avatar__photo avatar__photo--sm" src="/img/enig.png" /> <div class="avatar__intro"><div class="avatar__name">enigmalea</div><small class="avatar__subtitle"><a href="http://twitter.com/enigmaleaDA" class="footer__link-item"><i class="fa-brands fa-twitter"></i></a> <a href="http://enigmalea.tumblr.com" class="footer__link-item"><i class="fa-brands fa-tumblr"></i></a></small></div></div><div class="avatar"><img class="avatar__photo avatar__photo--sm" src="/img/fox.png" /> <div class="avatar__intro"><div class="avatar__name">foxinboots</div><small class="avatar__subtitle"><a href="http://twitter.com/TheFoxInBoots" class="footer__link-item"><i class="fa-brands fa-twitter"></i></a> <a href="http://thefoxinboots.tumblr.com" class="footer__link-item"><i class="fa-brands fa-tumblr"></i></a></small></div></div>',
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Arlathan eXchange. Built with Docusaurus.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-			metadata: [
-				{ name: "theme-color", content: "#2F3136" },
-				{ name: "twitter:card", content: "summary" },
-				{ name: "twitter:creator", content: "ArlathanXchange" },
-				{ name: "og:image", content: "/img/icon.png" },
-			],
-			zoom: {
-				selector: '.markdown img',
-				background: {
-				  light: 'rgb(255, 255, 255, 0.75)',
-				  dark: 'rgba(0, 0, 0, 0.75)'
-				},
-			},
-		}),
+                label: "Mod Team & Philosophy",
+                href: "/mod",
+              },
+              // {
+              //   label: "2023 Tag Set",
+              //   href: "https://archiveofourown.org/tag_sets/14455",
+              // },
+              // {
+              //   label: "2023 Nominations Sheet",
+              //   href: "https://docs.google.com/spreadsheets/d/1ruHKiXgFvfz54Q5m1K7wIMvVpbfI5k-_dk5mvxr9Sv8/edit?usp=sharing",
+              // },
+              // {
+              //   label: "2023 AutoAO3: Requests",
+              //   href: "https://autoao3app.fandom.tools/#/arlathanxchange2023/all",
+              // },
+              // {
+              //   label: "Request an Extension",
+              //   href: "/extension",
+              // },
+            ],
+          },
+          {
+            type: "dropdown",
+            label: "Tutorials",
+            position: "left",
+            className: "tutorial",
+            items: [
+              {
+                type: "doc",
+                label: "How to Nominate",
+                docId: "tutorials/nominate",
+              },
+              {
+                type: "doc",
+                label: "How to Sign-Up",
+                docId: "tutorials/signups",
+              },
+              {
+                type: "doc",
+                label: "On Exchange Letters",
+                docId: "tutorials/letters",
+              },
+              {
+                type: "doc",
+                label: "How to Filter the AutoAO3 App",
+                docId: "tutorials/autoao3",
+              },
+              {
+                type: "doc",
+                label: "How to Fulfill Your Assignment",
+                docId: "tutorials/fulfill",
+              },
+              {
+                type: "doc",
+                label: "Keeping It Anon",
+                docId: "tutorials/anon",
+              },
+            ],
+          },
+          {
+            type: "dropdown",
+            position: "left",
+            label: "AO3",
+            className: "ao3",
+            items: [
+              {
+                label: "Parent Collection",
+                href: "https://archiveofourown.org/collections/arlathanxchange/profile",
+                className: "ao3",
+              },
+              {
+                label: "2023 Collection",
+                href: "https://archiveofourown.org/collections/arlathanxchange2023/profile",
+                className: "ao3",
+              },
+              {
+                label: "2022 Collection",
+                href: "https://archiveofourown.org/collections/arlathanxchange2022/profile",
+                className: "ao3",
+              },
+            ],
+          },
+          {
+            type: "dropdown",
+            label: "Follow Us",
+            position: "left",
+            className: "follow",
+            items: [
+              {
+                href: "https://arlathanxchange.tumblr.com",
+                label: "Tumblr",
+                className: "tumblr",
+              },
+              {
+                href: "https://twitter.com/ArlathanXchange",
+                label: "Twitter",
+                className: "twitter",
+              },
+              {
+                href: "https://discord.gg/F73NDYMeGC",
+                label: "Discord",
+                className: "discord",
+              },
+            ],
+          },
+          // {
+          //   type: "dropdown",
+          //   label: "Announcements",
+          //   position: "left",
+          //   className: "blog",
+          //   items: [
+          //     {
+          //       label: "View",
+          //       href: "/blog",
+          //     },
+          //     {
+          //       label: "Follow via RSS",
+          //       href: "/blog/rss.xml",
+          //       className: "rss",
+          //     },
+          //   ],
+          // },
+        ],
+      },
+      footer: {
+        copyright: `Copyright © ${new Date().getFullYear()} ${copyright}`,
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+      prism: {
+        theme: lightTheme,
+        darkTheme: darkTheme,
+      },
+      metadata: [
+        { name: "theme-color", content: "#2F3136" },
+        { name: "twitter:card", content: "summary" },
+        { name: "twitter:creator", content: "ArlathanXchange" },
+        { name: "og:image", content: "/img/icon.png" },
+      ],
+      zoom: {
+        selector: ".markdown img",
+        background: {
+          light: "rgba(253, 247, 245, 0.75)",
+          dark: "rgba(15, 6, 50, 0.75)",
+        },
+      },
+    }),
 };
 
 module.exports = config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,23 @@
-import Countdown from '../components/Countdown';
-import HomepageFeatures from '../components/HomepageFeatures';
-import Layout from '@theme/Layout';
-import React from 'react';
-import styles from './index.module.css';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Countdown from "../components/Countdown";
+import HomepageFeatures from "../components/HomepageFeatures";
+import Layout from "@theme/Layout";
+import React from "react";
+import styles from "./index.module.css";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header>
-        <img src="/img/banner.png" className={styles.banner}></img>
+      <img src="/img/banner.png" className={styles.banner}></img>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      description="A Dragon Age fan exchange focused on a celebration of Elvhen characters.">
+    <Layout description="A Dragon Age fan exchange focused on a celebration of Elvhen characters.">
       <HomepageHeader />
       <main>
         <Countdown />
