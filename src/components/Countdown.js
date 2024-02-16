782adac3cb6a22faf6b546f2543bfcc2bc7b557b
enigmalea <enigmalea@gmail.com>
2024-02-15 23:16:54 -0500

    updated more 2024 info

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -4,7 +4,7 @@ import styles from "../pages/index.module.css";
 
 function Countdown() {
   const calculateTimeLeft = () => {
-    const difference = +new Date(`2023-06-04T12:00:00-04:00`) - +new Date();
+    const difference = +new Date(`2024-03-10T12:00:00-04:00`) - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -12,7 +12,6 @@ function Countdown() {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
       };
     }
 
@@ -43,16 +42,8 @@ function Countdown() {
   });
   return (
     <div className={styles.countdown}>
-      <h1>See you again in 2024 for Arlathan eXchange!</h1>
-      <b>
-        Join us for{" "}
-        <a href="https://dapolyshipping.neocities.org">DA Poly Exchange</a> in
-        Fall 2023
-        <br />
-        or join us at{" "}
-        <a href="https://thehangedmanpub.netlify.app/">The Hanged Man!</a>
-      </b>
-      {/* <b>{timerComponents.length ? timerComponents : <span>Now!</span>}</b> */}
+      <h2>Nominations Open:</h2>
+      <b>{timerComponents.length ? timerComponents : <span>Now!</span>}</b>
     </div>
   );
 }
