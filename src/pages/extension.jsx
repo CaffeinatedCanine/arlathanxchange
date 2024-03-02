8e71d2e15fd383832468c86ebaea5039c5a00186
enigmalea <enigmalea@gmail.com>
2024-03-02 09:19:45 -0500

    updated extension form and added link to ful page form.

--- src/pages/extension.jsx
+++ src/pages/extension.jsx
@@ -8,11 +8,21 @@ export default function Home() {
   return (
     <Layout description="Use this form to request an extension on your assignment for this year's exchange.">
       <main>
+        <p className={styles.note}>
+          If the form below is not accessible please{" "}
+          <a
+            href="https://airtable.com/appkPv96jtqYoBLz1/shrmg4RJQLFXOI6RF"
+            target="_blank"
+          >
+            click here
+          </a>{" "}
+          to go to a full-screen version.
+        </p>
         <iframe
           className={styles.form}
           src="https://airtable.com/embed/shrmg4RJQLFXOI6RF?backgroundColor=gray"
           width={500}
-          height={1215}
+          height={1065}
           style={{ background: "transparent", border: "0" }}
           title="An Airtable form to request an extension on your deadline for the exchange."
         ></iframe>
